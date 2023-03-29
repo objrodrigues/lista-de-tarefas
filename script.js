f5fee0abbcf68091ae3b181924717af8fd5e7f90
rdcodigo <rdcodigo@gmail.com>
2023-03-29 13:04:40 -0300

    add Local Storage Permanence

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-// Selection of elements
+// Captura de elementos --------------------------------------------------------------------------
 
-const todoForm = document.querySelector("#todo-form")
-const todoInput = document.querySelector("#todo-input")
-const todoList = document.querySelector("#todo-list")
+const taskForm = document.querySelector("#task-form")
+const taskInput = document.querySelector("#task-input")
+const tasksList = document.querySelector("#tasks-list")
 const editForm = document.querySelector("#edit-form")
 const editInput = document.querySelector("#edit-input")
 const cancelEditBtn = document.querySelector("#cancel-edit-btn")
@@ -10,117 +10,207 @@ const addEditBtn = document.querySelector("#add-edit-btn")
 const alert = document.querySelector("#alert")
 const closeAlertBtn = document.querySelector("#close-alert-btn")
 const editTaskAlert = document.querySelector(".edit-task-alert")
+
+// Variáveis --------------------------------------------------------------------------------------
 var taskElementEdit
-// --------------------------------------------------------------------------------------------------
 
-// Functions
-    const addTask = (title) => {
-        const task = document.createElement("div")
-        task.classList.add("task")
+// Funções ----------------------------------------------------------------------------------------
 
-        const taskTitle = document.createElement("h2")
-        taskTitle.classList.add("task-title")
-        taskTitle.innerText = title
+// Criar tarefa
+const createTask = (title, done = 0, saved = 1) => {
+    const task = document.createElement("div")
+    task.classList.add("task")
 
-        const taskOptionsContainer = document.createElement("section")
-        taskOptionsContainer.classList.add("task-options-container")
+    const taskTitle = document.createElement("h2")
+    taskTitle.classList.add("task-title")
+    taskTitle.innerText = title
 
-        const buttonDone = document.createElement("button")
-        buttonDone.setAttribute("class", "button-option")
-        buttonDone.innerHTML = '<img class="img-button-option button-done" src="./assets/done.svg" alt="">'
-        taskOptionsContainer.appendChild(buttonDone)
+    const taskOptionsContainer = document.createElement("section")
+    taskOptionsContainer.classList.add("task-options-container")
 
-        const buttonEdit = document.createElement("button")
-        buttonEdit.setAttribute("class", "button-option")
-        buttonEdit.innerHTML = '<img class="img-button-option button-edit" src="./assets/edit.svg" alt="">'
-        taskOptionsContainer.appendChild(buttonEdit)
+    const buttonDone = document.createElement("button")
+    buttonDone.setAttribute("class", "button-option")
+    buttonDone.innerHTML = '<img class="img-button-option button-done" src="./assets/done.svg" alt="">'
+    taskOptionsContainer.appendChild(buttonDone)
 
-        const buttonRemove = document.createElement("button")
-        buttonRemove.setAttribute("class", "button-option")
-        buttonRemove.innerHTML = '<img class="img-button-option button-remove" src="./assets/remove.svg" alt="">'
-        taskOptionsContainer.appendChild(buttonRemove)
-        
-        task.appendChild(taskTitle)
-        task.appendChild(taskOptionsContainer)
+    const buttonEdit = document.createElement("button")
+    buttonEdit.setAttribute("class", "button-option")
+    buttonEdit.innerHTML = '<img class="img-button-option button-edit" src="./assets/edit.svg" alt="">'
+    taskOptionsContainer.appendChild(buttonEdit)
 
-        todoList.appendChild(task)
+    const buttonRemove = document.createElement("button")
+    buttonRemove.setAttribute("class", "button-option")
+    buttonRemove.innerHTML = '<img class="img-button-option button-remove" src="./assets/remove.svg" alt="">'
+    taskOptionsContainer.appendChild(buttonRemove)
 
-        todoInput.value = ""
-        todoInput.focus()
+    task.appendChild(taskTitle)
+    task.appendChild(taskOptionsContainer)
+
+    if (done) {
+        task.classList.add("done")
     }
 
-    const closeAlert = () => {
-        alert.style.display = "none"
+    if (saved) {
+        saveTaskInLocalStorageTasksList({ title, done: 0 })
     }
-// --------------------------------------------------------------------------------------------------
 
-// Events
-todoForm.addEventListener(
+    tasksList.appendChild(task)
+
+    taskInput.value = ""
+}
+
+// Pegar lista de tarefas do Local Storage
+const getTasksListLocalStorage = () => {
+    const tasksList = JSON.parse(localStorage.getItem("tasksList")) || []
+    return tasksList
+}
+
+// Carregar lista de tarefas
+const loadTasksList = () => {
+    const tasksList = getTasksListLocalStorage()
+
+    tasksList.forEach(
+        (task) => {
+            createTask(task.title, task.done, 0)
+        }
+    )
+}
+
+// Salvar tarefa na lista de tarefas do Local Storage
+const saveTaskInLocalStorageTasksList = (task) => {
+    const tasksList = getTasksListLocalStorage()
+
+    tasksList.push(task)
+
+    localStorage.setItem("tasksList", JSON.stringify(tasksList))
+}
+
+// Remover tarefa da lista de tarefas do Local Storage
+const removeTaskInLocalStorageTasksList = (taskTitle) => {
+    const tasksList = getTasksListLocalStorage()
+
+    const filteredTasks = tasksList.filter((task) => task.title != taskTitle)
+
+    localStorage.setItem("tasksList", JSON.stringify(filteredTasks))
+}
+
+// Atualizar status da tarefa no Local Storage
+const updateTaskStatusLocalStorage = (taskTitle) => {
+    const tasksList = getTasksListLocalStorage()
+
+    tasksList.map(
+        (task) => task.title === taskTitle ? (task.done = !task.done) : null
+    )
+
+    localStorage.setItem("tasksList", JSON.stringify(tasksList))
+}
+
+// Atualizar a tarefa no Local Storage
+const updateTaskLocalStorage = (todoOldTitle, todoNewTitle) => {
+    const tasksList = getTasksListLocalStorage()
+
+    tasksList.map((task) =>
+        task.title === todoOldTitle ? (task.title = todoNewTitle) : null
+    )
+
+    localStorage.setItem("tasksList", JSON.stringify(tasksList))
+}
+
+const closeAlert = () => {
+    alert.style.display = "none"
+}
+
+// Eventos -------------------------------------------------------------------------------------------
+
+// Adicionar tarefa
+taskForm.addEventListener(
     "submit",
     (event) => {
         event.preventDefault()
-        const inputValue = todoInput.value
+        const inputValue = taskInput.value
 
-        if (inputValue){
-            addTask(inputValue)
-        console.log("Tarefa adicionada")
-        }else {
-         //   alert.style.display = "flex"
+        if (inputValue) {
+            createTask(inputValue)
         }
-        
     }
 )
 
+// Ações dos botões da tarefa
 document.addEventListener(
     "click",
     (event) => {
         const targetElement = event.target
         const taskElement = targetElement.closest("div")
 
-        if (targetElement.classList.contains("button-done")){
+        // Tarefa concluida
+        if (targetElement.classList.contains("button-done")) {
 
             taskElement.classList.add("done")
 
             const taskButton = targetElement.closest("button")
             taskButton.innerHTML = '<img class="img-button-option button-undone" src="./assets/undone.svg" alt="">'
 
-        }else if(targetElement.classList.contains("button-undone")){
+            taskTitle = taskElement.firstElementChild.innerText
+
+            updateTaskStatusLocalStorage(taskTitle)
+
+            // Tarefa não concluida
+        } else if (targetElement.classList.contains("button-undone")) {
 
             taskElement.classList.remove("done")
 
             const taskButton = targetElement.closest("button")
             taskButton.innerHTML = '<img class="img-button-option button-done" src="./assets/done.svg" alt="">'
 
-        }else if(targetElement.classList.contains("button-edit")){
+            taskTitle = taskElement.firstElementChild.innerText
+
+            updateTaskStatusLocalStorage(taskTitle)
+
+            // Abrir tela de edição de tarefa
+        } else if (targetElement.classList.contains("button-edit")) {
 
             taskElementEdit = taskElement
             editForm.style.display = "flex"
 
-        }else if(targetElement.classList.contains("button-remove")){
+            // Remover tarefa
+        } else if (targetElement.classList.contains("button-remove")) {
 
             taskElement.remove()
 
+            taskTitle = taskElement.firstElementChild.innerText
+
+            removeTaskInLocalStorageTasksList(taskTitle)
         }
     }
 )
 
-
+// Editar tarefa
 addEditBtn.addEventListener(
     "click",
     (event) => {
-        if(editInput.value){
+        if (editInput.value) {
             event.preventDefault()
-            taskElementEdit.querySelector("h2").innerText = editInput.value
+
+            let oldTitle = taskElementEdit.querySelector("h2").innerText
+            let newTitle = editInput.value
+
+            taskElementEdit.querySelector("h2").innerText = newTitle
+            
             editForm.style.display = "none"
             editTaskAlert.style.display = "none"
             editInput.value = ""
-        }else {
+
+            updateTaskLocalStorage(oldTitle, newTitle)
+
+            // Mostrar mensagens de campo inválido
+        } else {
             event.preventDefault()
             editTaskAlert.style.display = "flex"
         }
     }
 )
 
+// Fechar caixa de tarefa e ocultar mensagem de campo inválido
 cancelEditBtn.addEventListener(
     "click",
     (event) => {
@@ -129,4 +219,5 @@ cancelEditBtn.addEventListener(
         editTaskAlert.style.display = "none"
     }
 )
-// --------------------------------------------------------------------------------------------------
+
+loadTasksList()
