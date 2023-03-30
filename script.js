b245dd6a1074b542fed7a45effef49f925f9e4c2
rdcodigo <rdcodigo@gmail.com>
2023-03-29 21:45:37 -0300

    add filter function

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // Captura de elementos --------------------------------------------------------------------------
-
 const taskForm = document.querySelector("#task-form")
 const taskInput = document.querySelector("#task-input")
 const tasksList = document.querySelector("#tasks-list")
@@ -10,16 +9,24 @@ const addEditBtn = document.querySelector("#add-edit-btn")
 const alert = document.querySelector("#alert")
 const closeAlertBtn = document.querySelector("#close-alert-btn")
 const editTaskAlert = document.querySelector(".edit-task-alert")
+const selectFilterTask = document.querySelector("#filter-task")
 
 // Variáveis --------------------------------------------------------------------------------------
 var taskElementEdit
+var newtask
 
 // Funções ----------------------------------------------------------------------------------------
+//Recarregar lista de tarefas na tela
+const reloadTaskList = () => {
+    tasksList.innerHTML = null
+    loadFilteredTasksList(getFilterSelectedLocalStorage())
+}
 
-// Criar tarefa
-const createTask = (title, done = 0, saved = 1) => {
-    const task = document.createElement("div")
-    task.classList.add("task")
+// Criar nova tarefa
+const createTask = (title, done = false, isNewTask = true) => {
+
+    newTask = document.createElement("div")
+    newTask.classList.add("task")
 
     const taskTitle = document.createElement("h2")
     taskTitle.classList.add("task-title")
@@ -30,7 +37,14 @@ const createTask = (title, done = 0, saved = 1) => {
 
     const buttonDone = document.createElement("button")
     buttonDone.setAttribute("class", "button-option")
-    buttonDone.innerHTML = '<img class="img-button-option button-done" src="./assets/done.svg" alt="">'
+
+    if (!done){
+        buttonDone.innerHTML = '<img class="img-button-option button-done" src="./assets/done.svg" alt="">'
+    } else {
+        buttonDone.innerHTML = '<img class="img-button-option button-undone" src="./assets/undone.svg" alt="">'
+        newTask.classList.add("done")
+    }
+    
     taskOptionsContainer.appendChild(buttonDone)
 
     const buttonEdit = document.createElement("button")
@@ -43,40 +57,60 @@ const createTask = (title, done = 0, saved = 1) => {
     buttonRemove.innerHTML = '<img class="img-button-option button-remove" src="./assets/remove.svg" alt="">'
     taskOptionsContainer.appendChild(buttonRemove)
 
-    task.appendChild(taskTitle)
-    task.appendChild(taskOptionsContainer)
+    newTask.appendChild(taskTitle)
+    newTask.appendChild(taskOptionsContainer)
 
-    if (done) {
-        task.classList.add("done")
+    if (isNewTask){
+        saveTaskInLocalStorageTasksList({ title, done })
     }
-
-    if (saved) {
-        saveTaskInLocalStorageTasksList({ title, done: 0 })
-    }
-
-    tasksList.appendChild(task)
 
     taskInput.value = ""
+
+    return newTask
 }
 
 // Pegar lista de tarefas do Local Storage
 const getTasksListLocalStorage = () => {
-    const tasksList = JSON.parse(localStorage.getItem("tasksList")) || []
+    let tasksList = JSON.parse(localStorage.getItem("tasksList")) || []
     return tasksList
 }
 
-// Carregar lista de tarefas
-const loadTasksList = () => {
-    const tasksList = getTasksListLocalStorage()
-
-    tasksList.forEach(
-        (task) => {
-            createTask(task.title, task.done, 0)
-        }
-    )
+// Pegar filtro selecionado do Local Storage
+const getFilterSelectedLocalStorage = () => {
+    const filterSelected = localStorage.getItem("filter-task-selected")
+    return filterSelected
 }
 
-// Salvar tarefa na lista de tarefas do Local Storage
+// Carregar lista de tarefas filtrada na tela
+const loadFilteredTasksList = (filter) => {
+    let filteredTasksList
+
+    const loadTasksList = (element) => {
+        element.forEach(
+            (task) => {
+                tasksList.appendChild(createTask(task.title, task.done, false))
+            }
+        )
+    }
+
+    switch (filter) {
+        case 'all':
+            filteredTasksList = getTasksListLocalStorage()
+            loadTasksList(filteredTasksList)
+            break
+        case 'done':
+            filteredTasksList = getTasksListLocalStorage().filter(task => task.done === true)
+            loadTasksList(filteredTasksList)
+            break
+        case 'not-done':
+            filteredTasksList = getTasksListLocalStorage().filter(task => task.done === false)
+            loadTasksList(filteredTasksList)
+            break
+        default:
+    }
+}
+
+// Adicionar tarefa na lista de tarefas do Local Storage
 const saveTaskInLocalStorageTasksList = (task) => {
     const tasksList = getTasksListLocalStorage()
 
@@ -94,7 +128,7 @@ const removeTaskInLocalStorageTasksList = (taskTitle) => {
     localStorage.setItem("tasksList", JSON.stringify(filteredTasks))
 }
 
-// Atualizar status da tarefa no Local Storage
+// Atualizar status (Concluída / Não concluída) da tarefa no Local Storage
 const updateTaskStatusLocalStorage = (taskTitle) => {
     const tasksList = getTasksListLocalStorage()
 
@@ -105,12 +139,19 @@ const updateTaskStatusLocalStorage = (taskTitle) => {
     localStorage.setItem("tasksList", JSON.stringify(tasksList))
 }
 
-// Atualizar a tarefa no Local Storage
+// Atualizar título da tarefa no Local Storage
 const updateTaskLocalStorage = (todoOldTitle, todoNewTitle) => {
     const tasksList = getTasksListLocalStorage()
 
-    tasksList.map((task) =>
-        task.title === todoOldTitle ? (task.title = todoNewTitle) : null
+    tasksList.map(
+        (task) => {
+            if (task.title === todoOldTitle) {
+                task.title = todoNewTitle
+                task.done = false
+            } else {
+                null
+            }
+        }
     )
 
     localStorage.setItem("tasksList", JSON.stringify(tasksList))
@@ -121,7 +162,6 @@ const closeAlert = () => {
 }
 
 // Eventos -------------------------------------------------------------------------------------------
-
 // Adicionar tarefa
 taskForm.addEventListener(
     "submit",
@@ -131,6 +171,31 @@ taskForm.addEventListener(
 
         if (inputValue) {
             createTask(inputValue)
+        }
+
+        reloadTaskList()
+    }
+)
+
+// Filtrar tarefas
+selectFilterTask.addEventListener(
+    "change",
+    (event) => {
+        event.preventDefault()
+        tasksList.innerHTML = null
+        localStorage.setItem("filter-task-selected", selectFilterTask.value)
+
+        switch (getFilterSelectedLocalStorage()) {
+            case 'all':
+                loadFilteredTasksList("all")
+                break
+            case 'done':
+                loadFilteredTasksList("done")
+                break
+            case 'not-done':
+                loadFilteredTasksList("not-done")
+                break
+            default:
         }
     }
 )
@@ -142,7 +207,7 @@ document.addEventListener(
         const targetElement = event.target
         const taskElement = targetElement.closest("div")
 
-        // Tarefa concluida
+        // Modificar status da tarefa para concluída
         if (targetElement.classList.contains("button-done")) {
 
             taskElement.classList.add("done")
@@ -154,7 +219,7 @@ document.addEventListener(
 
             updateTaskStatusLocalStorage(taskTitle)
 
-            // Tarefa não concluida
+            // Modificar status da tarefa para não concluída
         } else if (targetElement.classList.contains("button-undone")) {
 
             taskElement.classList.remove("done")
@@ -195,14 +260,16 @@ addEditBtn.addEventListener(
             let newTitle = editInput.value
 
             taskElementEdit.querySelector("h2").innerText = newTitle
-            
+
             editForm.style.display = "none"
             editTaskAlert.style.display = "none"
             editInput.value = ""
 
             updateTaskLocalStorage(oldTitle, newTitle)
 
-            // Mostrar mensagens de campo inválido
+            reloadTaskList()
+
+            // Mostrar mensagem de campo inválido
         } else {
             event.preventDefault()
             editTaskAlert.style.display = "flex"
@@ -220,4 +287,6 @@ cancelEditBtn.addEventListener(
     }
 )
 
-loadTasksList()
+localStorage.setItem("filter-task-selected", selectFilterTask.value)
+
+reloadTaskList()
